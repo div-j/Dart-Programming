03d291649a657e7ae8b5553fe179891632d87286
John Divine <74967859+div-j@users.noreply.github.com>
2023-12-12 13:44:14 +0100

    dart input and output

---
+++ Dart IO/inputAndOutput.dart
@@ -0,0 +1,22 @@
+  //Dart I/O (Input and Output)
+
+void main(){
+  '''
+  Dart provides access to the standard input and output streams 
+  using stdin and stdout from the dart:io library.
+  ''';
+  
+  void greet(){
+  print('Please enter your name: ');
+  var student = stdin.readLineSync();
+  print('Hello);
+  }
+  
+ greet();
+  
+'''
+The input except value from the user
+while the print output, present or display values to the use
+''';
+  
+}
